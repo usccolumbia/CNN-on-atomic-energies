c2098a3ddf524ecc356f2ac0701ea1601d738f50
Simula Kristoffer <simulak1@kosh.org.aalto.fi>
2018-01-23 16:03:15 +0200

    main.py now produces theano shared variables out of CM:s in a flattened shape.

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import load_data
 import plotting
 import time
 import matplotlib.pyplot as plt
+import random as rd
+
+rd.seed(23455)
+
+Ndata=2400
+Ntrain=2000
+Nval=Ndata-Ntrain
+
+alpha  = 0.001   # Adam Learning rate
+Nepoch = 20      # Number of epochs
+Nf     = [20,20] # Number of filters in each convlayer
+mbs    = 50      # minibatch size
+reg    = 0.0001  # Regularization parameter
 
 ###### Location of data ######
 datapath = '/u/82/simulak1/unix/Desktop/kurssit/deep_learning/project/data'
@@ -10,8 +23,6 @@ filename = datapath+'/train.csv'
 ############################## 
 
 #######  Get all available raw data ###### 
-Ntrain=2400
-
 print("*** Loading raw data ***")
 start=time.time()
 spacegrp,Natoms,pc_al,pc_ga,pc_in,lv_alpha,lv_beta,lv_gamma,lvadeg,lvbdeg,lvgdeg,Ef,Eg = load_data.get_train_data(filename)
@@ -30,12 +41,28 @@ print("Time taken to load geometry data: "+str(end-start)+"\n")
 
 ###### Get all the relevant data for training ######
 print('*** Loading datapoints ***')
-Xtrain=np.zeros((Ntrain,80,80))
+Xtrain=np.zeros((Ntrain,6400))
 Ytrain=np.zeros((Ntrain,))
 for i in np.arange(Ntrain):
-    Xtrain[i,:,:]=np.loadtxt('CM/'+str(i+1)+'.gz')
+    X=np.loadtxt('CM/'+str(i+1)+'.gz')
+    Xtrain[i,:]=X.flatten(0)
     Ytrain[i]=Ef[i]
+Xval=np.zeros((Nval,6400))
+Yval=np.zeros((Nval,))
+for i in np.arange(Nval):
+    X=np.loadtxt('CM/'+str(Ntrain+i+1)+'.gz')
+    Xval[i,:]=X.flatten(0)
+    Yval[i]=Ef[i]
 timing=time.time()
 print('Time taken to load data: '+str(timing-end))
 ####################################################
+
+###### Turn data into theano shared variables ##################
+valid_set_x, valid_set_y, valid_set = load_data.shared_dataset(
+    Xval, Yval,
+    sample_size=Nval)
+train_set_x, train_set_y, train_set = load_data.shared_dataset(
+    Xtrain, Ytrain,
+    sample_size=Ntrain)
+###############################################################
 
