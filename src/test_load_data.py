5691c4c3aeea68faf76df507bada53b10698b613
Kristoffer Simula <krsimula@taito-gpu.csc.fi>
2018-02-14 11:32:56 +0200

    shared_dataset-function handles now also vectors as labels, and test function is added

--- src/test_load_data.py
+++ src/test_load_data.py
@@ -22,7 +22,9 @@ def test_shared_dataset():
     # Below AB is not tested, it returns list of input arrays
     A=np.ones((2,2))
     B=np.ones((2,1))
+    C=np.ones((2,))
     As,Bs,AB=load_data.shared_dataset(A,B,sample_size=2)
+    As,Cs,AC=load_data.shared_dataset(A,B,sample_size=2)
     same=True
     for i in range(2):
         for j in range(2):
@@ -30,7 +32,12 @@ def test_shared_dataset():
                 same=False
     for i in range(2):
         if(np.abs(B[i,0]-Bs.get_value(borrow=True)[i,0])>0.01):
-               same=False
+            same=False
+    for i in range(2):
+        if(np.abs(C[i]-Cs.get_value(borrow=True)[i])>0.01):
+            same=False
+           
+    
     assert same
             
 
