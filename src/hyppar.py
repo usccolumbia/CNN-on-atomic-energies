377cdc7115a3089c9996d5e98a5088af64a3ae7d
Kristoffer Simula <krsimula@taito-gpu.csc.fi>
2018-02-22 11:59:14 +0200

    Now number of convlayers can be any from 1-7. Instead of defining parameters separately for each convlayer, this is done in a loop. New filters, pools and activations parsed.
    TODO: generalize input for arbitrary number of layerTODO: generalize input for arbitrary number of layers.s.

--- src/hyppar.py
+++ src/hyppar.py
@@ -22,22 +22,38 @@ reg = 0.001
 # 2D input
 in_x = 80
 in_y = 80
+in_z = 1 
 # Number of convolutional layers
 NCL = 3
 # Number of chanels
-Nchannel        =  [8, 8, 8]
+Nchannel        =  [1, 8, 8, 8]
 # Filters
 filter1         = [9, 9]
 filter2         = [5, 5]
 filter3         = [5, 5]
+filter4         = [5, 5] 
+filter5         = [5, 5] 
+filter6         = [5, 5] 
+filter7         = [5, 5] 
+
 # Pooling
 pool1          = [2, 2]
 pool2          = [2, 2]
 pool3          = [2, 2]
+pool4          = [2, 2]
+pool5          = [2, 2]
+pool6          = [2, 2]
+pool7          = [2, 2]
+
 # Activations  
 activation1    = 'tanh'
 activation2    = 'tanh'
 activation3    = 'tanh'
+activation4    = 'tanh'
+activation5    = 'tanh'
+activation6    = 'tanh'
+activation7    = 'tanh' 
+
 # Number of fully connected layers
 NFC            = 1
 # Activations
@@ -71,99 +87,81 @@ def setStructureParameters():
     global activation3
     global fc_activation
     global Nchannel
+    
+    # New global parameters:
+    global image_spec_x
+    global image_spec_y
+    
+    global pool
+    pool = []
+    pool.append(pool1)
+    pool.append(pool2)
+    pool.append(pool3)
+    pool.append(pool4)
+    pool.append(pool5)
+    pool.append(pool6)
+    pool.append(pool7)
 
-    if NCL > 0:
-        # Layer 2 input size x
-        global image_spec_2_x
-        image_spec_2_x = in_x-filter1[0]+1
-        if (ignore_border):
-            image_spec_2_x=(image_spec_2_x-image_spec_2_x%pool1[0])/pool1[0]
-        else:
-            image_spec_2_x=(image_spec_2_x+image_spec_2_x%pool1[0])/pool1[0]
-        # Layer 2 input size y
-        global image_spec_2_y
-        image_spec_2_y = in_y-filter1[1]+1
-        if (ignore_border):
-            image_spec_2_y=(image_spec_2_y-image_spec_2_y%pool1[1])/pool1[1]
-        else:
-            image_spec_2_y=(image_spec_2_y+image_spec_2_y%pool1[1])/pool1[1]
+    global filter
+    filter = []
+    filter.append(filter1)
+    filter.append(filter2)
+    filter.append(filter3)
+    filter.append(filter4)
+    filter.append(filter5)
+    filter.append(filter6)
+    filter.append(filter7)
 
-    if NCL > 1:
-        # Layer 3 input size x                                                                                   
-        global image_spec_3_x
-        image_spec_3_x = image_spec_2_x-filter2[0]+1
-        if (ignore_border):
-            image_spec_3_x=(image_spec_3_x-image_spec_3_x%pool2[0])/pool2[0]
-        else:
-            image_spec_3_x=(image_spec_3_x+image_spec_3_x%pool2[0])/pool2[0]
-        # Layer 3 input size y                                                                               
-        global image_spec_3_y
-        image_spec_3_y = image_spec_2_y-filter2[1]+1
-        if (ignore_border):
-            image_spec_3_y=(image_spec_3_y-image_spec_3_y%pool2[1])/pool2[1]
-        else:
-            image_spec_3_y=(image_spec_3_y+image_spec_3_y%pool2[1])/pool2[1]
+    global activation
+    activation = []
+    activation.append(activation1)
+    activation.append(activation2)
+    activation.append(activation3)
+    activation.append(activation4)
+    activation.append(activation5)
+    activation.append(activation6)
+    activation.append(activation7)
 
-    global fc_in_x
-    global fc_in_y
-    if NCL==1:
-        fc_in_x=image_spec_2_x
-        fc_in_y=image_spec_2_y
-    elif NCL==2:
-        fc_in_x=image_spec_3_x
-        fc_in_y=image_spec_3_y
-    elif NCL==3:
-        fc_in_x = image_spec_3_x-filter3[0]+1
-        if (ignore_border):
-            fc_in_x=(fc_in_x-fc_in_x%pool3[0])/pool3[0]
-        else:
-            fc_in_x=(fc_in_x+fc_in_x%pool3[0])/pool3[0]
-        fc_in_y = image_spec_3_y-filter3[1]+1
-        if (ignore_border):
-            fc_in_y=(fc_in_y-fc_in_y%pool3[1])/pool3[1]
-        else:
-            fc_in_y=(fc_in_y+fc_in_y%pool3[1])/pool3[1]
 
+    image_spec_x = []
+    image_spec_y = []
+
+    image_spec_x.append(in_x)
+    image_spec_y.append(in_y)
+
+    for i in range(1,NCL+1):
+        image_spec_x.append(image_spec_x[i-1]-filter[i-1][0]+1)
+        image_spec_y.append(image_spec_y[i-1]-filter[i-1][1]+1)
+        if (image_spec_x[i] < 1 or image_spec_y[i] < 1):
+            print("\n ERROR!!! Too large filter in convlayer "+str(i)+" \n")
+        if (ignore_border):
+            image_spec_x[i]=(image_spec_x[i]-image_spec_x[i]%pool[i-1][0])/pool[i-1][0]
+            image_spec_y[i]=(image_spec_y[i]-image_spec_y[i]%pool[i-1][1])/pool[i-1][1]
+        else:
+            image_spec_x[i]=(image_spec_x[i]+image_spec_x[i]%pool[i-1][0])/pool[i-1][0]
+            image_spec_y[i]=(image_spec_y[i]+image_spec_y[i]%pool[i-1][1])/pool[i-1][1]
 
     print('\n STRUCTURE OF NETWORK')
     print('\n Number of convolutional layers      : '+str(NCL))
     print(' Number of fully connected layers    : '+str(NFC))
-    
+
     print('\n Shape of the input:')
     print(in_x,in_y)
     print('\n')
-    print("*** Convlayer 1: ***")
-    print('Filter shape:') # Generalize!!!!!!!!!!!!                                                  
-    print(filter1[0],filter1[1])
-    print(' Pooling:')
-    print(pool1[0],pool1[1])
-    print(' Activation: ')
-    print(activation1)
-    print('Output image1:')
-    print(Nchannel[0],image_spec_2_x,image_spec_2_y)
-    print('\n')
 
-    print("*** Convlayer 2: ***")
-    print('\n Filter shape:')
-    print(filter2[0],filter2[1])
-    print(' Pooling:')
-    print(pool2[0],pool2[1])
-    print(' Activation:')
-    print(activation2)
-    print('Output image:')
-    print(Nchannel[1],image_spec_3_x,image_spec_3_y)
-    print('\n')
-
-    print("*** Convlayer 3: ***")
-    print('\n Filter shape')
-    print(filter3[0],filter3[1])
-    print(' Pooling.')
-    print(pool3[0],pool3[1])
-    print(' Activation: ')
-    print(activation3)
-    print('Output image:')
-    print(Nchannel[2],fc_in_x,fc_in_y)
-
+    for i in range(NCL):    
+    
+        print("*** Convlayer "+ str(i+1)+" ***")
+        print('Filter shape:') # Generalize!!!!!!!!!!!!                                                  
+        print(filter[i][0],filter[i][1])
+        print(' Pooling:')
+        print(pool[i][0],pool[i][1])
+        print(' Activation: ')
+        print(activation[i])
+        print('Output image1:')
+        print(Nchannel[i],image_spec_x[i+1],image_spec_y[i+1])
+        print('\n')
+        
     print(' Fully connected layer activation   : '+fc_activation)
         
 
@@ -217,7 +215,46 @@ def setInput(filename='input'):
     global fc_activation
     global ignore_border
 
-
+    # Declare (again) the default values
+    learning_rate = 0.05
+    Nepoch = 10
+    mbs = 20
+    reg = 0.001
+    
+    in_x = 80
+    in_y = 80
+    in_z = 1
+    NCL = 3
+    Nchannel        =  [1, 8, 8, 8]
+    filter1         = [9, 9]
+    filter2         = [5, 5]
+    filter3         = [5, 5]
+    filter4         = [5, 5]
+    filter5         = [5, 5]
+    filter6         = [5, 5]
+    filter7         = [5, 5]
+    
+    pool1          = [2, 2]
+    pool2          = [2, 2]
+    pool3          = [2, 2]
+    pool4          = [2, 2]
+    pool5          = [2, 2]
+    pool6          = [2, 2]
+    pool7          = [2, 2]
+    
+    activation1    = 'tanh'
+    activation2    = 'tanh'
+    activation3    = 'tanh'
+    activation4    = 'tanh'
+    activation5    = 'tanh'
+    activation6    = 'tanh'
+    activation7    = 'tanh'
+    
+    NFC            = 1
+    fc_activation  = 'lin'
+    
+    
+    
     datapath_buffer = parse(filename,'datapath')
     if len(datapath_buffer)>0:
         datapath = datapath_buffer[0]
@@ -250,11 +287,6 @@ def setInput(filename='input'):
     if len(mbs_buffer) > 0:
         mbs           = int(mbs_buffer[0])
 
-    Nchannel_buffer = parse(filename,'Nchannel')
-    if len(Nchannel_buffer) > 0:
-        for i in range(len(Nchannel)):
-            Nchannel[i]    = int(Nchannel_buffer[i])
-
     reg_buffer      = parse(filename,'reg')
     if len(reg_buffer) > 0:
         reg             = float(reg_buffer[0])
@@ -266,6 +298,10 @@ def setInput(filename='input'):
     buffer = parse(filename,'in_y')
     if len(buffer) > 0:
         in_y = int(buffer[0])
+        
+    buffer = parse(filename,'in_z')
+    if len(buffer) > 0:
+        in_z = int(buffer[0])
     
     buffer = parse(filename,'NCL')
     if len(buffer) > 0:
@@ -273,8 +309,9 @@ def setInput(filename='input'):
 
     buffer = parse(filename,'Nchannel')
     if len(buffer) > 0:
-        for i in range(NCL):
-            Nchannel[i] = int(buffer[i])
+        Nchannel[0] = in_z
+        for i in range(1,NCL+1):
+            Nchannel[i] = int(buffer[i-1])
 
     buffer = parse(filename,'filter1')
     if len(buffer) > 0:
@@ -291,6 +328,26 @@ def setInput(filename='input'):
         for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
             filter3[i] = int(buffer[i])
 
+    buffer = parse(filename,'filter4')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D
+            filter4[i] = int(buffer[i])
+
+    buffer = parse(filename,'filter5')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+             filter5[i] = int(buffer[i])
+
+    buffer = parse(filename,'filter6')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+            filter6[i] = int(buffer[i])
+
+    buffer = parse(filename,'filter7')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+            filter7[i] = int(buffer[i])
+
     buffer = parse(filename,'pool1')
     if len(buffer) > 0:
         for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
@@ -306,6 +363,27 @@ def setInput(filename='input'):
         for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
             pool3[i] = int(buffer[i])
 
+    buffer = parse(filename,'pool4')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+            pool4[i] = int(buffer[i])
+    
+    buffer = parse(filename,'pool5')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+            pool5[i] = int(buffer[i])
+
+    buffer = parse(filename,'pool6')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+            pool6[i] = int(buffer[i])
+    
+    buffer = parse(filename,'pool7')
+    if len(buffer) > 0:
+        for i in range(2): # ! This can be easily generalized to 1 or 3D                                         
+            pool7[i] = int(buffer[i])
+
+
     buffer = parse(filename,'activation1')
     if len(buffer) > 0:
         activation1=buffer[0]
@@ -317,6 +395,23 @@ def setInput(filename='input'):
     buffer = parse(filename,'activation3')
     if len(buffer) > 0:
         activation3=buffer[0]
+
+    buffer = parse(filename,'activation4')
+    if len(buffer) > 0:
+        activation4=buffer[0]
+
+    buffer = parse(filename,'activation5')
+    if len(buffer) > 0:
+        activation5=buffer[0]
+
+    buffer = parse(filename,'activation6')
+    if len(buffer) > 0:
+        activation6=buffer[0]
+
+    buffer = parse(filename,'activation7')
+    if len(buffer) > 0:
+        activation7=buffer[0]
+
 
     buffer = parse(filename,'fc_activation')
     if len(buffer) > 0:
